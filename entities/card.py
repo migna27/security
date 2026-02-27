9efdc8435739a29b47587f9bb3324bcce9d4f046
Miguel <gosvaldo372@gmail.com>
2026-02-27 09:55:31 -0700

    Card

---
+++ entities/card.py
@@ -0,0 +1,43 @@
+from secutiry.crypto import encrypt, decrypt
+from persistence.db import get_connection
+
+
+class Card:
+    def __init__(self, id, number, type, id_usuario, Banco):
+        self.id = id
+        self.number = number
+        self.type = type
+        self.id_usuario = id_usuario
+
+
+    def card_insert(number, type, id_usuario, Banco):
+        connection = get_connection()
+        cursor = connection.cursor()
+        number_encrypt = encrypt(number)
+        sql = "INSERT INTO card(number, type, id_usuario, Banco) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (number_encrypt, type, id_usuario, Banco))
+        connection.commit()
+        cursor.close()
+        connection.close()
+    
+
+    def get_cards_by_user_id(id_usuario):
+        connection = get_connection()
+        cursor = connection.cursor(dictionary=True)
+        sql = "SELECT id, number, type, id_usuario, Banco, FROM card WHERE id_usuario = %s"
+        cursor.execute(sql, (id_usuario,))
+        rows = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return [
+            Card(
+                id=row["id"],
+                number=decrypt(row["number"]),
+                type=row["type"],
+                id_usuario=row["id_usuario"],
+                Banco=row["Banco"]
+
+            )
+            for row in rows
+        ]
+    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from entities.user import User
 from getpass import getpass
-
+from entities.card import Card
 def registrar_user():
     name= input("nombre: ")
     curp= input("CURP: ")
@@ -40,11 +40,40 @@ if __name__=="__main__":
         print(" Seleccione una opción del menú")
         print(" 1.-Registrar un usuario")
         print(" 2.-Consultar un usuario")
+        print (" 3.-Registrar una tarjeta")
+        print(" 4.-Consultar una tarjetas")
         option= int(input())
         if option ==1:
             registrar_user()
         elif option==2:
             view_users()
+        elif option==3:
+            number= input("Número de la tarjeta: ")
+            type= input("Tipo de la tarjeta: 1= débito, 2= crédito: ")
+            if type == "1":
+                type= "Debit"
+            elif type == "2":
+                type= "Credit"
+            else:
+                print("Opción inválida")
+        
+            view_users()
+            id_usuario= input("ID del usuario: ")
+            Banco= input("Banco: ")
+            Card.card_insert(number, type, id_usuario, Banco)
+            print("Tarjeta registrada exitosamente")
+
+        elif option==4:
+            user_id= input("ID del usuario: ")
+            cards= Card.get_cards_by_user_id(user_id)
+            for card in cards:
+                print(f"ID: {card.id}, Number: {card.number}, Type: {card.type}, Banco: {card.Banco}")
+
+
+            
+        else:
+            print("Opción inválida")
+
     else:
         print("Usuario inválido")
    
